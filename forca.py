89b6ee9feb467e0f21edfabb4a4062d7bfcd1d93
p4b1it0 <cabreir@outlook.com>
2022-04-23 17:12:13 -0300

    Melhorando o código através da criação de funções.

--- forca.py
+++ forca.py
@@ -1,10 +1,68 @@
 import random
 
+
 def jogar():
+
+    imprime_mensagem_inicial()
+
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    # Enquanto não enforcou E não acertou - Este é o Game Loop
+    while(not enforcou and not acertou):
+
+        chute = captura_chute()
+
+        if(chute in palavra_secreta):
+            valida_chute_correto(palavra_secreta, chute, letras_acertadas)
+        else:
+            erros += 1
+
+        print(letras_acertadas)
+
+        if("_" not in letras_acertadas):
+            imprime_mensagem_ganhou()
+            break
+        elif(erros == 6):
+            imprime_mensagem_perdeu()
+            break
+        else:
+            continue
+
+    print("Fim do jogo!")
+
+# Funções
+
+def imprime_mensagem_ganhou():
+    print("Parabéns, você ganhou!")
+
+def imprime_mensagem_perdeu():
+    print("Não foi desta vez!")
+
+def valida_chute_correto(palavra_secreta, chute, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):  # aqui compartamos a entrada sempre como se fosse letra maíuscula
+            # aqui guardamos as letras existentes na lista
+            letras_acertadas[index] = letra
+        index += 1
+
+def captura_chute():
+    chute = input("Digite uma letra: ")
+    chute = chute.strip().upper()
+    return chute
+
+def imprime_mensagem_inicial():
     print("================================")
     print("===Bem-vindo ao Jogo da Forca===")
     print("================================")
 
+def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -16,43 +74,10 @@ def jogar():
 
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero]
+    return palavra_secreta
 
-    # palavra_secreta = "banana".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]  # List Comprehension
-
-    enforcou = False
-    acertou = False
-    erros = 0
-
-    # Enquanto não enforcou E não acertou - Este é o Game Loop
-    while(not enforcou and not acertou):
-
-        chute = input("Digite uma letra: ")
-        # estamos tratando a variável para limpar espaços em branco
-        chute = chute.strip().upper()
-
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):  # aqui compartamos a entrada sempre como se fosse letra maíuscula
-                    # aqui guardamos as letras existentes na lista
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-
-        print(letras_acertadas)
-
-        if("_" not in letras_acertadas):
-            print("Parabéns, você ganhou!")
-            break
-        elif(erros == 6):
-            print("Não foi desta vez!")
-        else:
-            continue
-
-    print("Fim do jogo!")
-
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 if(__name__ == "__main__"):
     jogar()
